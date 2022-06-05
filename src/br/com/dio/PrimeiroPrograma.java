36f6751ef350388a6051f0a4401122423ca7f669
Robson Leal <devleal@hotmail.com>
2022-06-05 12:43:30 -0300

    Criando classe livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -4,9 +4,28 @@ import br.com.dio.model.Gato;
 public class PrimeiroPrograma {
     public static void main(String[] args) {
         Gato gato = new Gato("Make", "Branco", 2);
+        Livro livro1 = new Livro("O último guerreiro", 300);
 
         gato.setIdade(1);
 
         System.out.println(gato);
+        System.out.println(livro1);
+    }
+}
+class Livro {
+    private String nome;
+    private Integer numPaginas;
+
+    public Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
     }
 }
